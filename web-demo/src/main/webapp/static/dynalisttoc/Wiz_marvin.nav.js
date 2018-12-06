a4c4a58d8aeb723af76cbd26ed27f7f0c6890c07
StickChen <chen_xuanlong@163.com>
2018-12-06 14:34:42 +0800

    toc

--- web-demo/src/main/webapp/static/dynalisttoc/Wiz_marvin.nav.js
+++ web-demo/src/main/webapp/static/dynalisttoc/Wiz_marvin.nav.js
@@ -1,12 +1,17 @@
 $(function () {
 
-    let intervalScriptBody = setInterval(intervalBody, 3000);
+    let intervalScriptBody = setInterval(intervalBody, 5000);
+    let isScrollSpy = 0;
 
     function intervalBody() {
         if($('.Node-children').length > 1){
             scriptBody();
             clearInterval(intervalScriptBody);
             intervalScriptBody = null;
+            if(isScrollSpy === 1) {
+                scrollSpy();
+            }
+            ++isScrollSpy;
         }
     }
 
@@ -14,7 +19,7 @@ $(function () {
 
     var observer = new MutationObserver(function(mutations, observer) {
         if(!intervalScriptBody) {
-            // intervalScriptBody = setInterval(intervalBody, 5000);
+            intervalScriptBody = setInterval(intervalBody, 10000);
         }
     });
 
@@ -29,9 +34,14 @@ $(function () {
 function scriptBody(){
     console.log("dynalist toc");
     if($('#TocContainer').length === 0) {
-        $('.normal-view').before('<div id="TocContainer"></div>');
+        $('.normal-view').before('<div id="TocContainer"><div id="sideToolbarContainer" class="cssVisibility"></div><a href="javascript:void(0);" id="sideCatalogBtn" ></a></div>');
+        // 显示隐藏目录
+        $('#sideCatalogBtn').on('click', function () {
+            $("#sideToolbarContainer").toggleClass('cssVisibility')
+            $(this).toggleClass('sideCatalogBtnDisable')
+        });
     }else {
-        $('#TocContainer').empty();
+        $('#sideToolbarContainer').empty();
     }
 
     // 需将主体宽度减小
@@ -44,7 +54,7 @@ function scriptBody(){
         catalog = 'sideCatalog-catalog',
         catalogBtn = 'sideCatalogBtn',
         sideToolbarUp = 'sideToolbar-up',
-        i = '<div id="sideToolbar" style="display:block;">\<div class="sideCatalogBg" id="sideCatalog">\<div id="sideCatalog-sidebar">\<div class="sideCatalog-sidebar-top"></div>\<div class="sideCatalog-sidebar-bottom"></div>\</div>\<div id="sideCatalog-catalog">\<ul class="nav"style="zoom:1">\</ul>\</div>\</div>\</div><a href="javascript:void(0);" id="sideCatalogBtn" ></a>',
+        i = '<div id="sideToolbar" style="display:block;">\<div class="sideCatalogBg" id="sideCatalog">\<div id="sideCatalog-sidebar">\<div class="sideCatalog-sidebar-top"></div>\<div class="sideCatalog-sidebar-bottom"></div>\</div>\<div id="sideCatalog-catalog">\<ul class="nav"style="zoom:1">\</ul>\</div>\</div>\</div>',
         ulHtml = '',
         k = 0,
         l1 = 0,
@@ -54,32 +64,34 @@ function scriptBody(){
     if (body.length === 0) {
         return
     }
-    $('#TocContainer').append(i);
+    $('#sideToolbarContainer').append(i);
     let lv1Selector = 'div.Document-rootNode > div > div.Node-children > div.Node-outer';
     let titleSelector = 'div.Node-self > div.node-line.Node-contentContainer > div.Node-renderedContent.node-line > span';
     let childrenSelector = 'div.Node-children > div.Node-outer > div';
     body.find(lv1Selector)
         .each(function (i) {
             l1++;
+            l2=0;
             var headerContainer1 = $(this).children();
             var header1 = $(headerContainer1.find(titleSelector)[0]);
             var title1 = header1.text();
             var text1 = header1.text();
             header1.attr('id', 'autoid-' + l1 + '-' + l2 + '-' + l3);
-            if (text1.length > 14) text1 = text1.substr(0, 12) + "...";
+            // if (text1.length > 14) text1 = text1.substr(0, 12) + "...";
 
-            ulHtml += '<li><span>' + l1 + '&nbsp&nbsp</span><a href="#' + header1.attr('id') + '" title="' + title1 + '">' + text1 + '</a><span class="sideCatalog-dot"></span></li>';
+            ulHtml += '<li><span>' + l1 + '&nbsp&nbsp</span><a id="a_' + header1.attr('id') + '" href="#' + header1.attr('id') + '" title="' + title1 + '">' + text1 + '</a><span class="sideCatalog-dot"></span></li>';
 
             $(headerContainer1.children()[1]).children()
                 .each(function (i) {
                     l2++;
+                    l3=0;
                     var headerContainer2 = $(this).children();
                     var header2 = $(headerContainer2.find(titleSelector)[0]);
                     var title2 = header2.text();
                     var text2 = header2.text();
                     header2.attr('id', 'autoid-' + l1 + '-' + l2 + '-' + l3);
-                    if (text2.length > 14) text2 = text2.substr(0, 12) + "...";
-                    ulHtml += '<li class="h2Offset"><span>' + l1 + '.' + l2 + '&nbsp&nbsp</span><a href="#' + header2.attr('id') + '" title="' + title2 + '">' + text2 + '</a></li>';
+                    // if (text2.length > 14) text2 = text2.substr(0, 12) + "...";
+                    ulHtml += '<li class="h2Offset"><span>' + l1 + '.' + l2 + '&nbsp&nbsp</span><a id="a_' + header2.attr('id') + '" href="#' + header2.attr('id') + '" title="' + title2 + '">' + text2 + '</a></li>';
 
                     $(headerContainer2.children()[1]).children()
                         .each(function (i) {
@@ -89,9 +101,8 @@ function scriptBody(){
                             var title3 = header3.text();
                             var text3 = header3.text();
                             header3.attr('id', 'autoid-' + l1 + '-' + l2 + '-' + l3);
-                            if (text3.length > 14) text3 = text3.substr(0, 12) + "...";
-                            ulHtml += '<li class="h3Offset"><span>' + l1 + '.' + l2 + '.' + l3 + '&nbsp&nbsp</span><a href="#' + header3.attr('id') + '" title="' + title3 + '">' + text3 + '</a></li>';
-
+                            // if (text3.length > 14) text3 = text3.substr(0, 12) + "...";
+                            ulHtml += '<li class="h3Offset"><span>' + l1 + '.' + l2 + '.' + l3 + '&nbsp&nbsp</span><a id="a_' + header3.attr('id') + '" href="#' + header3.attr('id') + '" title="' + title3 + '">' + text3 + '</a></li>';
                         });
                 });
         });
@@ -105,21 +116,12 @@ function scriptBody(){
     //     target: '.sideCatalogBg'
     // });
 
-    // 显示隐藏目录
-    $sideToolbar = $('#' + sideToolbar);
-    $('#' + catalogBtn).on('click', function () {
-        if ($(this).hasClass('sideCatalogBtnDisable')) {
-            $sideToolbar.css('visibility', 'hidden')
-        } else {
-            $sideToolbar.css('visibility', 'visible')
-        }
-        $(this).toggleClass('sideCatalogBtnDisable')
-    });
-    $('#' + sideToolbarUp).on('click', function () {
-        $("html,body").animate({
-            scrollTop: 0
-        }, 500)
-    });
+
+    // $('#' + sideToolbarUp).on('click', function () {
+    //     $("html,body").animate({
+    //         scrollTop: 0
+    //     }, 500)
+    // });
     // var $sideToolbar = $('#' + sideToolbar);
     // a.on('scroll', function () {
     //     var t = a.scrollTop();
@@ -130,4 +132,58 @@ function scriptBody(){
     //     }
     // })
 
+
+    // 可调整大小
+    $("#sideToolbarContainer").resizable({
+        // handleSelector: ".splitter",
+        resizeHeight: false
+    });
+
+    // $("#sideCatalog-catalog").scrollspy();
+
+
 };
+
+function scrollSpy() {
+    let $DocumentContainer = $('.DocumentContainer');
+    $(function domReady($) {
+        // 对所有内容
+        $('div.Node-self > div.node-line.Node-contentContainer > div.Node-renderedContent.node-line > span').each(function eachElement() {
+            // cache the jQuery object
+            var $this = $(this);
+
+            var position = $this.position();
+
+            // window.console.log(position);
+            // window.console.log('min: ' + position.top + ' / max: ' + window.parseInt(position.top + $this.height(), 10));
+
+            $this.scrollspy({
+                container: $DocumentContainer,
+                min: position.top,
+                max: position.top + $this.height(),
+                onEnter: function onEnter(element/*, position*/) {
+                    // window.console.log('Entering ' + element.id);
+
+                    let href = $('#a_' + element.id);
+                    let offset = href.parent().offset();
+                    let scrollTop = $('#sideToolbar').scrollTop();
+                    if(offset && offset.top) {
+                        let top = offset.top;
+
+                        if(top > 900){
+                            $('#sideToolbar').scrollTop(top - 200);
+                        }
+                        if(top < 0){
+                            $('#sideToolbar').scrollTop(scrollTop - 300);
+                        }
+                    }
+                    href.parent().addClass('active');
+                },
+                onLeave: function onLeave(element/*, position*/) {
+                    // window.console.log('Leaving ' + element.id);
+                    $('li > a[href*="' + element.id + '"]').parent().removeClass('active');
+                }
+            });
+        });
+    });
+}
