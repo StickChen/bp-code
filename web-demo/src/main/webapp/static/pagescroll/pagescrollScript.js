86d911f263717435e180affd0d829b3fc81b4721
StickChen <chen_xuanlong@163.com>
2018-12-29 00:19:55 +0800

    pageScroll

--- web-demo/src/main/webapp/static/pagescroll/pagescrollScript.js
+++ web-demo/src/main/webapp/static/pagescroll/pagescrollScript.js
@@ -7,9 +7,9 @@
 // @match        *://*/*
 // @require      http://cdn.bootcss.com/jquery/3.2.1/jquery.min.js
 //== @resource     css file:\\D:\WorkProject\!CXL\bp-code\web-demo\src\main\webapp\static\pagescroll\pagescroll.css
-// @resource     css https://gitcdn.xyz/repo/StickChen/bp-code/master/web-demo/src/main/webapp/static/dynalisttoc/pagescroll.css
+// @resource     css https://gitcdn.xyz/repo/StickChen/bp-code/master/web-demo/src/main/webapp/static/pagescroll/pagescroll.css
 //== @require     file:\\D:\WorkProject\!CXL\bp-code\web-demo\src\main\webapp\static\pagescroll\pagescroll.js
-// @require     https://gitcdn.xyz/repo/StickChen/bp-code/master/web-demo/src/main/webapp/static/dynalisttoc/pagescroll.js
+// @require     https://gitcdn.xyz/repo/StickChen/bp-code/master/web-demo/src/main/webapp/static/pagescroll/pagescroll.js
 // @grant        none
 // ==/UserScript==
 
